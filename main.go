9ed87171b52276f9a00d942e6adaebb27bf81cf8
federus1105 <federusrudi@gmail.com>
2025-10-21 19:53:16 +0700

    Feat ( Main ) : add function where the number 66 is, then 88 is added before the number 66

--- main.go
+++ main.go
@@ -4,16 +4,26 @@ import "fmt"
 
 func main() {
 	scores := []int{50, 75, 66, 20, 32, 90}
-
 	result := []int{}
 
+	// TASK 1
 	result = append(result, scores[:3]...)
-	fmt.Println(result)
 	result = append(result, 88)
-	fmt.Println(result)
 	result = append(result, scores[3:]...)
 
 	for x := range len(result) {
 		fmt.Println(result[x])
+	}
+
+	// TASK 2
+	for i := 0; i < len(scores); i++ {
+		if scores[i] == 66 {
+			result = append(result, 88)
+		}
+		result = append(result, scores[i])
+	}
+
+	for _, val := range result {
+		fmt.Println(val)
 	}
 }
